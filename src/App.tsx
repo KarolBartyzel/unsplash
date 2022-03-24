f14c2f774f7899b51ea071cdac4e127acf42d70b
Karol Bartyzel <karolbartyzel308@gmail.com>
2022-03-24 14:35:57 +0100

    Init repo from template

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header"></header>
+      <main className="app-main"></main>
+      <footer className="app-footer"></footer>
+    </div>
+  );
+}
+
+export default App;
